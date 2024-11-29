4cf8dcfff79416311036c3cb0b80bb64ae38391a
masterlord123 <michaelluis.suacito.cics@ust.edu.ph>
2024-11-29 19:31:20 +0800

    push cleaned_line_item_data.csv line_item_data_prices_pipeline

---
+++ Cleaned Dataset/Operations Department/Pipelines/line_item_data_prices_pipelines.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+prices1 = pd.read_csv('line_item_data_prices1.csv')
+prices2 = pd.read_csv('line_item_data_prices2.csv')
+prices3 = pd.read_parquet('line_item_data_prices3.parquet')
+
+prices = [prices1, prices2, prices3]
+allprice = pd.concat(prices)
+
+# transform quantity column
+allprice['quantity'] = allprice['quantity'].str.extract(r'(\d+)').astype(int)
+
+#drop Unnamed: 0 cols
+allprice = allprice.drop('Unnamed: 0', axis=1)
+
+#make csv
+allprice.to_csv("cleaned_line_item_data_prices.csv", index=False)
